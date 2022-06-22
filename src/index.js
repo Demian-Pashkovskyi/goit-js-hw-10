6fcd4860ccf9c48cfe016b02798cd0624b15dbd6
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-22 11:48:43 +0300

    import бібліотек

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 const DEBOUNCE_DELAY = 300;
+const debounce = require('lodash.debounce');
+
+
