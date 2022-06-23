72c04ca55402af9027eced19640724a4d090e236
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-23 13:27:05 +0300

    hw-10 js вибірка країн

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries';
+// import { fetchCountries } from './js/fetchCountries';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -8,3 +8,19 @@ const DEBOUNCE_DELAY = 300;
 const debounce = require('lodash.debounce');
 
 
+// https://restcountries.com/v3.1/name/{name}
+
+function fetchCountries(name) {
+	const filter = `?fields=name,capital,population,flags,languages`;
+	return fetch(`https://restcountries.com/v3.1/name/${name}?${filter}`).then(response => response.json())
+}
+fetchCountries("Canada").then(country => {
+	console.log(country);
+});
+
+const searchBox = document.querySelector("#search-box");
+
+searchBox.addEventListener("submit", event => {
+	event.preventDefault();
+	console.log(searchBox.elements.country);
+})
